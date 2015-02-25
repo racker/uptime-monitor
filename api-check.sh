20709f11e1a0aa6e25f65bab70813e302fd680d5
Eddie Sheffield <eddie.sheffield@rackspace.com>
2015-02-25 16:55:04 -0500

    Small bug fix

--- api-check.sh
+++ api-check.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-PYTHONPATH=$PYTHONPATH:${DIR}
+export PYTHONPATH=$PYTHONPATH:${DIR}
 python -m uptimemonitor.agentplugin.uptimeplugin \
         -a $1 -u $2 -k $3 -t $4 -r $5
