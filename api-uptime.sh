6e8aa2fc65f6e087491f7f490bbe0ba1d004a5c2
Eddie Sheffield <eddie.sheffield@rackspace.com>
2015-03-04 16:39:12 -0500

    Removed the uptime calc agent plugin

--- api-uptime.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export PYTHONPATH=$PYTHONPATH:${DIR}
-python -m uptimemonitor.agentplugin.nova_uptime_plugin \
-        -u $1 -k $2 -r $3 -w $4
